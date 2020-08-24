ecdf3c9ab87b4fd97c24be64073c6cb8ccdbee10
Lyumih <m.ivanov1225@gmail.com>
2020-08-24 17:50:31 +0300

    feat(project): add button buy

--- app/components/PageFooter/index.js
+++ app/components/PageFooter/index.js
@@ -6,7 +6,7 @@
 
 import React from 'react';
 import { github } from 'utils/constants';
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
 import styled from 'styled-components';
 
 import { StickyFooter } from './sticky';
@@ -14,10 +14,11 @@ import { StickyFooter } from './sticky';
 
 const Footer = StickyFooter.extend`
   textAlign: center;
-  background: #efeeee;
+  background: #1105fd;
   color: #5a5a5a;
   padding: 10px;
   font-size: 14px;
+  height: 100%;
 `;
 
 const Span = styled.span`
@@ -27,9 +28,24 @@ const Span = styled.span`
 
 function PageFooter() {
   return (
-    <Footer>
+    <Footer >
       <Row>
-        <Col sm={12} xs={24}>
+        <Col>
+        <form action="https://www.coinpayments.net/index.php" method="post">
+          <input type="hidden" name="cmd" value="_pay"/>
+          <input type="hidden" name="reset" value="1"/>
+          <input type="hidden" name="merchant" value="86afdb1ad036c067549f4860191c8660"/>
+          <input type="hidden" name="item_name" value="KRG Token"/>
+          <input type="hidden" name="currency" value="USD"/>
+          <input type="hidden" name="amountf" value="1.00000000"/>
+          <input type="hidden" name="quantity" value="1"/>
+          <input type="hidden" name="allow_quantity" value="1"/>
+          <input type="hidden" name="want_shipping" value="1"/>
+          <input type="hidden" name="allow_extra" value="0"/>
+          <input type="image" src="https://www.coinpayments.net/images/pub/buynow-wide-blue.png" alt="Купить используя CoinPayments.net"/>
+        </form>
+        </Col>
+        {/* <Col sm={12} xs={24}>
           {'ETH Hot Wallet - '}
           <a href={github} target="_blank" rel="noopener">
             Ethereum Wallet with ERC20 support (GitHub)
@@ -45,7 +61,7 @@ function PageFooter() {
             <br />
             ETH: 0x97325941fafde5a182e6f7e5475a592ac615a3f2
           </Col>
-        </Span>
+        </Span> */}
 
       </Row>
     </Footer>
